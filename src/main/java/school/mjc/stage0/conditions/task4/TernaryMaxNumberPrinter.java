f6b6e6c36eb744b62236c8f18536d0e41f40c6ca
Sergey234 <mostov.sergey88@gmail.com>
2022-11-08 18:26:25 +0300

    stage0-module4-conditions-task4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int ternaryMaxNumberPrinterOfFirstAndSecondNumbers = first >= second ? first : second;
+            if(ternaryMaxNumberPrinterOfFirstAndSecondNumbers >= first || ternaryMaxNumberPrinterOfFirstAndSecondNumbers >= second
+                    || ternaryMaxNumberPrinterOfFirstAndSecondNumbers >= third) {
+                System.out.println(ternaryMaxNumberPrinterOfFirstAndSecondNumbers);
+            } else if (ternaryMaxNumberPrinterOfFirstAndSecondNumbers <= third) {
+                System.out.println(third);
+            }
     }
 }
